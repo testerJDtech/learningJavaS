c9727feb82bb612df7911e3f084dbce3f40d779a
John Dada <johndada@Mac.home>
2025-05-28 22:24:02 +0100

    learnt how to link js file to html file using src= in the script tag

---
+++ app.js
@@ -0,0 +1,8 @@
+// const means that the variable remains constant and cannot be reassigned
+// good to use const when I know the value for the variable will not change    
+const name = "Nike";
+
+// use console.log to print messages to the console on the browser
+// This outputs whatever is passed in through the browser console
+console.log(name);
+console.log(name + " is a meady brand!");
