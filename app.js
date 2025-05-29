afccf1db046f8e5b738c8b63baeeead0cf10c176
John Dada <johndada@Mac.home>
2025-05-29 09:31:45 +0100

    learnt about and how to use conditional statements and comparison operators

--- app.js
+++ app.js
@@ -6,3 +6,41 @@ const name = "Nike";
 // This outputs whatever is passed in through the browser console
 console.log(name);
 console.log(name + " is a meady brand!");
+
+
+// Learning how to use condiitonal statements // if else statements
+let totalPrice = 19;
+let shippingCost;
+
+// whatever is inside the if statement is called the condiition. It is either goign to be met or not met
+if (totalPrice > 20) {
+    shippingCost = 0;
+} else {
+    shippingCost = 5;
+}
+
+if (totalPrice <= 20) {
+    shippingCost = 5;
+// You can't write conditional statements in else statements
+} else {
+    shippingCost = 0;
+}
+
+if (totalPrice <= 10) {
+    shippingCost = 5;
+// you can write conditional statements in else if statements as well
+} else if (totalPrice <= 20) {
+    shippingCost = 3;
+} else {
+    shippingCost = 0;
+}
+
+let userMembership = "gold";
+
+if (userMembership !== "gold") {
+    // show user non-premium content
+    // recommend premium membership
+} else {
+    // show premium content
+    console.log("Welcome to the premium content section!");
+}
